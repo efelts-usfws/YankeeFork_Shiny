bfc30627f7a3667486962125a8f6d03526bba4b6
efelts-usfws <eli_felts@fws.gov>
2025-04-28 14:32:24 -0700

    Incorporated flow plot and cumulative plot into the shiny app

--- app.R
+++ app.R
@@ -23,9 +23,15 @@ flow.dat <- read_feather("data/yfk_flow")
 
 daily.dat <- read_feather("data/daily")
 
+
+
+
 individuals.dat <- read_feather("data/individuals")
 
 slider_min <- as.Date(min(individuals.dat$yfk_entry_final))
+
+lastweek <- individuals.dat %>% 
+  filter(yfk_entry_final>=today()-days(7))
 
 
 user_dates <-     
@@ -68,7 +74,36 @@ ui <- page_navbar(
                 
                 title="Unique PIT Tagged Steelhead, SY 2025",
                 value=nrow(individuals.dat),
-                showcase=fa("fish-fins")
+                showcase=fa("fish-fins"),
+                
+             
+                ),
+              
+              value_box(
+                title="New in the Last Week",
+                value=nrow(lastweek),
+                showcase=bs_icon("graph-up-arrow")
+                
+                
+                
+              )
+              
+            ),
+            
+            page_fillable(
+              
+              
+              layout_columns(
+                
+                col_widths= c(6,6,6),
+                
+                card(card_header("Stream Discharge"),
+                     plotlyOutput("flow_plot"),
+                     full_screen = T),
+                
+                card(card_header("Year-to-date Totals"),
+                     plotlyOutput("comp_plot"),
+                     full_screen = T)
                 
                 
               )
@@ -84,7 +119,73 @@ ui <- page_navbar(
 
 server <- function(input,output,session){
   
+  # make the flow plot as a reactive
   
+  flowplot_reactive <- reactive({
+    
+    plot_min <- min(input$user_dates)
+    plot_max <- max(input$user_dates)
+    
+    flow.plot <- flow.dat %>% 
+      mutate(date=as_date(date)) %>% 
+      ggplot(aes(x=date,y=mean_discharge,group=group))+
+      geom_line(aes(text=str_c(" Date:",date,
+                               "<br>","Mean Discharge (cfs): ",mean_discharge,
+                               sep=" ")))+
+      scale_x_date(date_breaks = "1 month", date_labels="%b",
+                   limits=c(as.Date(plot_min),as.Date(plot_max)))+
+      theme_bw()+
+      labs(x="",y="Mean Discharge at Yankee Fork Gaging Station")
+    
+  })
+  
+  # Render the flow plot as a plotly object
+  
+  output$flow_plot <- renderPlotly({
+    
+    plot1 <- flowplot_reactive()
+    
+    ggplotly(plot1,
+             tooltip=c("text"))
+    
+  })
+  
+  # make a reactive plot of cumulative
+  # numbers in; this is named comp plot bc
+  # eventually it will compare the cumulative
+  # curve among years, right now it will
+  # just be showing current year (2025)
+  
+  compplot_reactive <- reactive({
+    
+    plot_lim.dat <- tibble(min_doy=min(input$user_dates),
+                           max_doy=max(input$user_dates))
+    
+    comp_plot <- daily.dat %>% 
+      ggplot(aes(x=yfk_final_date,y=cumulative_total,
+                 group=spawn_year))+
+      geom_line(aes(text=str_c(" Date:",format(yfk_final_date, "%b %d"),
+                               "<br>",
+                               "Number In:",round(cumulative_total),sep=" ")))+
+      theme_bw()+
+       scale_x_date(date_breaks = "1 month", date_labels="%b",
+                    limits=c(as.Date(plot_lim.dat$min_doy),as.Date(plot_lim.dat$max_doy)))+
+      labs(x="Date to Yankee Fork Salmon River",
+           y="# PIT Tags in Yankee Fork, Year-To-Date",
+           color="")
+    
+  })
+  
+  # Render the comp plot as a plotly object
+  
+  output$comp_plot <- renderPlotly({
+    
+    plot1 <- compplot_reactive()
+    
+    ggplotly(plot1,
+             tooltip=c("text")) 
+    
+  })
 }
 
 shinyApp(ui, server)
