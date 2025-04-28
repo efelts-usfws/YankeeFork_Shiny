ecf007b43c16044099081cf5f494a8236925d18c
efelts-usfws <eli_felts@fws.gov>
2025-04-28 13:22:00 -0700

    Finished adapting the current season processing code to Yankee Fork, will still need to get the prior years summarized to estimate things like what percentage of the run is complete, etc.

--- R/ptagis_processing.R
+++ R/ptagis_processing.R
@@ -102,3 +102,156 @@ int.dat <- dat %>%
 write.table(int.dat,"data/inseason_int.txt",
             quote=FALSE,row.names=FALSE,col.names=FALSE)
 
+# another part to automate, run
+# that output text file through the 
+# complete tag history in PTAGIS
+# and export the results to the
+# taghistory folder
+
+# make a key for life stage at tagging
+# based on pit id
+
+lifestage.key <- dat %>% 
+  group_by(pit_id) %>% 
+  summarize(life_stage=first(release_lifestage))
+
+# now read in the interrogation results for the adult-marked
+# and compress to unique day/event site combos
+
+inseason.int <- read_csv("data/Complete Tag History.csv") %>% 
+  mutate(observation_datetime=as.POSIXct(`Event Date Time Value`,
+                                         format = "%m/%d/%Y %I:%M:%S %p", 
+                                         tz = "America/Los_Angeles"),
+         observation_date=as_date(observation_datetime)) %>% 
+  select(pit_id=`Tag Code`,obs_type=`Event Type Name`,
+         observation_sitecode=`Event Site Code Value`,
+         observation_datetime,observation_date,
+         length_mm=`Event Length mm`) %>% 
+  group_by(pit_id,observation_date,observation_sitecode) %>% 
+  slice(which.min(observation_datetime)) %>% 
+  left_join(lifestage.key,by="pit_id")
+
+
+ptagis.dat <- read_feather("shapes/ptagis_sites")%>% 
+  group_by(site_code) %>% 
+  slice(which.max(total_rkm))
+
+# also list out site code ids for interrogation
+# sites that indicate the South Fork overall,
+# and those that are at the mouth so detections
+# are interpreted as entry into South Fork
+
+yfk_logical <- c("YFK","YANKFK")
+
+yfkentry_logical <- c("YFK")
+
+# So now split out and deal with juveniles
+
+juv.dat1 <- inseason.int %>% 
+  filter(life_stage=="Juvenile",
+         obs_type=="Observation") %>% 
+  left_join(ptagis.dat,by=c("observation_sitecode"="site_code")) %>% 
+  group_by(pit_id,observation_datetime) %>%
+  arrange(pit_id,observation_datetime) %>% 
+  group_by(pit_id) %>% 
+  mutate(last_site=last(observation_sitecode),
+         lowest_rkm=min(total_rkm))
+
+
+# So now filter for those records that are fish that were detected
+# at least as far down as lower granite and their last detection was in 
+# the south fork
+
+juv.filter <- juv.dat1  %>% 
+  filter(lowest_rkm<=695,
+         last_site %in% yfk_logical)  %>% 
+  group_by(pit_id,observation_date,observation_sitecode) %>% 
+  slice(which.min(observation_datetime)) %>% 
+  select(pit_id,obs_type,observation_sitecode,
+         observation_datetime,observation_date) %>% 
+  mutate(length_mm=as.numeric(NA),
+         mark_stage="Juvenile")  
+
+# pull out the adults from the initial int data
+# distinguish their mark stage then bind
+# to the filtered juvenile data
+
+int.complete <- inseason.int %>% 
+  filter(life_stage=="Adult") %>% 
+  rename(mark_stage=life_stage) %>% 
+  bind_rows(juv.filter)
+
+# now summarize relevant values and pull
+# in marking location as well
+
+dat.mark <- dat %>% 
+  group_by(pit_id) %>%
+  summarize(release_sitecode=first(release_sitecode),
+            release_datetime=first(release_datetime))
+
+yfk_individuals.summary <- int.complete %>% 
+  mutate(yfk=ifelse(observation_sitecode %in% yfk_logical,TRUE,
+                   FALSE),
+         yfk_entry=ifelse(observation_sitecode %in% yfkentry_logical,
+                         TRUE,FALSE)) %>% 
+  group_by(pit_id) %>% 
+  summarize(lgr_final=last(observation_datetime[observation_sitecode=="GRA"]),
+            yfk_first=first(observation_datetime[yfk==TRUE]),
+            yfk_entry_final=last(observation_datetime[yfk_entry==TRUE]),
+            yfk_diff=as.numeric(yfk_entry_final-yfk_first,units="days"),
+            length_mm=mean(length_mm,na.rm=T),
+            mark_stage=first(mark_stage)) %>% 
+  left_join(dat.mark,by="pit_id")
+
+
+yfk_entry.summary <- yfk_individuals.summary %>% 
+  mutate(yfk_final_date=as_date(yfk_entry_final)) %>% 
+  group_by(yfk_final_date) %>% 
+  summarise(n=n()) %>% 
+  ungroup() %>% 
+  mutate(sy_total=sum(n),
+         cumulative_total=cumsum(n),
+         daily_prop=n/sy_total,
+         daily_cumulative=cumsum(daily_prop),
+         spawn_year=2025)
+
+# now also grab yfk water data from USGS gaging station
+
+yfk.site <- "13296000"
+
+
+# temp is coded as 00010, discharge as 00060,
+# we'll go for both of those
+
+parm.cd <- c("00010","00060")
+
+
+# put today's date into text format
+# to feed into the query of daily
+# water data
+
+today.text <- as.character(today(tz="America/Los_Angeles"))
+
+yfk.daily <- readNWISdv(siteNumber=yfk.site,
+                           parameterCd = parm.cd,
+                           startDate="1990-01-01",
+                           endDate=today.text) %>% 
+  select(date=Date,
+         mean_temp=X_00010_00003,
+         mean_discharge=X_00060_00003) %>% 
+  mutate(date=as_date(date))
+
+yfk.dat <-yfk.daily %>% 
+  filter(date>=as_date("2025-01-01"),
+         date<=today()) %>% 
+  mutate(group=1)
+
+write_feather(yfk.dat,"data/yfk_flow")
+
+
+write_feather(yfk_individuals.summary,
+              "data/individuals")
+
+write_feather(yfk_entry.summary,
+              "data/daily")
+
