258380353814b1418adce57b8682ef6fbc7a1da9
efelts-usfws <eli_felts@fws.gov>
2025-05-19 11:43:24 -0700

    adding chinook previous year's comparison and creating according outputs

--- comparison_build.R
+++ comparison_build.R
@@ -158,13 +158,11 @@ yfkindividuals_completedyrs <- summary.dat |>
             yfk_diff=as.numeric(yfk_entry_final-yfk_first,units="days"),
             length_mm=mean(length_mm,na.rm=T),
             release_lifestage=first(release_lifestage)) |> 
-  left_join(dat.mark,by="pit_id")
+  left_join(dat.mark,by="pit_id") |> 
+  mutate(species="Steelhead")
 
 
-# that's the data frame that will be used to plot so save here
 
-saveRDS(yfkindividuals_completedyrs,
-        "data/individuals_completed")
 
 # now summarize by day so we can reference how much of 
 # the run should be complete on a given day
@@ -189,3 +187,94 @@ yfk_entry_daily <- yfkindividuals_completedyrs |>
 
 saveRDS(yfk_entry_daily,
         "data/daily_completed")
+
+
+# add in Chinook
+
+chn.dat <- read_csv("data/YFK CHN_Comparisons.csv") |>  
+  mutate(observation_sitecode=word(`Site Name`,1,sep=" "),
+         release_sitecode=word(`Release Site Name`,1,sep=" "),
+         observation_datetime=as.POSIXct(`Obs Time Value`,
+                                         format = "%m/%d/%Y %I:%M:%S %p", 
+                                         tz = "America/Los_Angeles"),
+         observation_month=month(observation_datetime),
+         observation_year=year(observation_datetime),
+         spawn_year=observation_year,
+         release_datetime=mdy(`Release Date MMDDYYYY`),
+         release_year=year(release_datetime),
+         yrs_at_large=observation_year-release_year) |> 
+  select(pit_id=`Tag Code`,rear_type=`Rear Type Code`,
+         run_type=`Run Name`,
+         release_sitecode,release_lifestage=`Mark Life Stage Value`,
+         release_datetime,release_year,
+         observation_sitecode,
+         observation_datetime,observation_month,
+         observation_year,yrs_at_large,
+         spawn_year,
+         length_mm=`Mark Length mm`) |> 
+  mutate(most_recent=max(spawn_year,na.rm=T),
+         release_lifestage=case_when(
+           
+           is.na(release_lifestage) & is.na(length_mm) ~ "Juvenile",
+           is.na(release_lifestage) & length_mm <= 300 ~ "Juvenile",
+           is.na(release_lifestage) & length_mm > 300 ~ "Adult",
+           TRUE ~ release_lifestage
+           
+         )) |> 
+  filter(!(yrs_at_large==0 & release_lifestage=="Juvenile"),
+         !(yrs_at_large==1 & release_sitecode%in% c("YANKFK","YANKWF")))
+
+
+chn_yfk_downstream.filter <- read_csv("data/YFK Chinook Downstream Comparisons.csv") |> 
+  mutate(ds_datetime=as.POSIXct(`Obs Time Value`,
+                                format = "%m/%d/%Y %I:%M:%S %p", 
+                                tz = "America/Los_Angeles"),
+         ds_site=word(`Site Name`,1,sep=" ")) |> 
+  select(pit_id=`Tag Code`,
+         ds_datetime,ds_site) |> 
+  group_by(pit_id) |> 
+  slice(which.max(ds_datetime))
+
+chn_yfk_juvenile.filter <- chn.dat |> 
+  filter(release_lifestage=="Juvenile") |> 
+  left_join(chn_yfk_downstream.filter,by="pit_id") |> 
+  filter(!is.na(ds_datetime)) |> 
+  group_by(pit_id) |> 
+  filter(observation_datetime>ds_datetime) |> 
+  ungroup() |> 
+  select(-c(ds_datetime,ds_site))
+
+chn_summary.dat <- chn.dat |> 
+  filter(release_lifestage=="Adult"|
+           (release_lifestage=="Juvenile"&
+              !release_sitecode %in% c("YANKFK","YANKWF"))) |> 
+  bind_rows(chn_yfk_juvenile.filter)
+
+chn_dat.mark <- chn_summary.dat |> 
+  group_by(pit_id) |> 
+  summarize(release_sitecode=first(release_sitecode),
+            release_datetime=first(release_datetime))
+  
+
+chn_yfkindividuals_completedyrs <- chn_summary.dat |> 
+  group_by(pit_id,spawn_year) |>
+  summarize(yfk_first=first(observation_datetime),
+            yfk_entry_final=last(observation_datetime),
+            yfk_diff=as.numeric(yfk_entry_final-yfk_first,units="days"),
+            length_mm=mean(length_mm,na.rm=T),
+            release_lifestage=first(release_lifestage)) |> 
+  left_join(chn_dat.mark,by="pit_id") |> 
+  mutate(species="Chinook")
+
+# individuals completed will be used in Shiny, 
+# so want species and then bind and save 
+# as an RDS
+
+# that's the data frame that will be used to plot so save here
+
+
+yfkindividuals_completedyrs_bind <- bind_rows(yfkindividuals_completedyrs,
+                                              chn_yfkindividuals_completedyrs)
+
+saveRDS(yfkindividuals_completedyrs_bind,
+        "data/individuals_completed_bind")
